1e7473dd1b17910e40a9d6f6349165c0cfce7f67
Manoj Singh Bisht <155564605+Manojsinghbisht08@users.noreply.github.com>
2025-02-02 18:16:22 +0530

    Delete random.js

--- random.js
+++
@@ -1,4 +0,0 @@
- const manoj =['manoj is smart boy','manoj is so handsome','manoj is a very rich person'];
- const neetu=Math.floor(Math.random() * messages.length);
- nettumanoj=mssg['neetu'];
- console.log('nettumanoj');
